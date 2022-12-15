2371ba9a1c1bf46367ce6482b9809253851e0e17
notjik <notjik@yandex.ru>
2022-12-15 17:13:23 +0500

    add README.md & requirements.txt && debugging

---
+++ README.md
@@ -0,0 +1,39 @@
+###### Espresso-Cappuccino-Latte-Macchiato
+
+<details><summary> 
+<strong>English</strong>
+</summary>
+
+# Espresso-Cappuccino-Latte-Macchiato
+
+### Collecting several tasks from the second year of Yandex Lyceum
+###### Generalization: You need to create an application with a UI interface in which you can fill in information about coffee. 
+###### Fields: Name, Roasting, Type, Taste, Price, Size.
+
+##### Interaction with the program is carried out through the external interface, which opens after the program is launched 
+##### Launch example:
+
+```commandline
+python main.py
+```
+
+</details>
+
+<details><summary> 
+<strong>Русский</strong>
+</summary>
+
+# Эспрессо-Капучино-Латте-Макиато
+
+### Сбор нескольких заданий со второго курса Яндекс Лицея
+###### Обобщение: Необходимо создать приложение с UI интерфейсом, в котором можно будет заполнять информацию о кофе. 
+###### Поля: Имя, Обжарка, Тип, Вкус, Цена, Размер.
+
+##### Взаимодействие с программой осуществляется через внешний интерфейс, который открывается после запуска программы
+##### Пример запуска:
+
+```commandline
+python main.py
+```
+
+</details>
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import sys
+import traceback
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox, QDialog
@@ -9,28 +10,36 @@ class EditDB(QDialog):
     def __init__(self):
         super(EditDB, self).__init__()
         uic.loadUi("addEditCoffeeForm.ui", self)
-        self.buttonBox.clicked.connect(self.run)
 
-    def run(self):
+    def accept(self) -> None:
         con = sqlite3.connect('coffee.sqlite')
         cur = con.cursor()
         cur.execute('INSERT INTO coffee(Name, Roasting, Type, Taste, Price, Size) VALUES (?,?,?,?,?,?)',
-                    [self.NameLineEdit, self.RoastingLineEdit, self.TypeLineEdit,
-                     self.TasteLineEdit, self.PriceLineEdit, self.SizeLineEdit])
+                    [self.NameLineEdit.text(), self.RoastingLineEdit.text(), self.TypeLineEdit.text(),
+                     self.TasteLineEdit.text(), self.PriceLineEdit.text(), self.SizeLineEdit.text()])
         con.commit()
-        self.NameLineEdit.setText('')
-        self.RoastingLineEdit.setText('')
-        self.TypeLineEdit.setText('')
-        self.TasteLineEdit.setText('')
-        self.PriceLineEdit.setText('')
-        self.SizeLineEdit.setText('')
-        QMessageBox.information(self, "Готово!", "Информация сохранена.", QMessageBox.Ok)
+        con.close()
+        self.done(0)
+
+    def reject(self) -> None:
+        self.done(0)
 
 
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         uic.loadUi("main.ui", self)
+        self.table()
+        self.addInfoButton.clicked.connect(self.run)
+
+    def run(self) -> None:
+        sys.excepthook = except_hook
+        EDB = EditDB()
+        EDB.show()
+        EDB.exec()
+        self.table()
+
+    def table(self) -> None:
         con = sqlite3.connect('coffee.sqlite')
         cur = con.cursor()
         cur.execute(f"""SELECT * FROM coffee""")
@@ -41,18 +50,13 @@ class Window(QMainWindow):
         for i, elemi in enumerate(db):
             for j, elemj in enumerate(elemi[1:]):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(elemj))
-        self.addInfoButton.clicked.connect(self.run)
-
-    def run(self):
-        app2 = QApplication(sys.argv)
-        sys.excepthook = except_hook
-        EDB = EditDB()
-        EDB.show()
-        app2.exec()
+        con.close()
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+
+def except_hook(cls, exception, trace):
+    tb = "".join(traceback.format_exception(cls, exception, trace))
+    print(tb)
 
 
 # Точка входа (Entry point)
